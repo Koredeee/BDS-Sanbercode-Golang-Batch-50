175172971a127e12fffc65ca8555d4926124289e
Koredeee <m.fatihdaffa@gmail.com>
2023-10-04 12:59:26 +0700

    Added Tugas-2

---
+++ Tugas-2/tugas.go
@@ -0,0 +1,33 @@
+// Program to illustrate fmt.Print()
+
+package main
+
+// import the fmt package
+import "fmt"
+
+func main() {
+
+	// soal 1
+	golanggo := "Bootcamp Digital Skill Sanbercode Golang"
+	fmt.Print(golanggo)
+
+	// soal 2
+	halo := "Halo Golang"
+	fmt.Print(halo)
+
+	// soal 3
+	var kataPertama = "saya"
+	var kataKedua = "senang"
+	var kataKetiga = "belajar"
+	var kataKeempat = "golang"
+
+	fmt.Print(kataPertama)
+	fmt.Print(kataKedua)
+	fmt.Print(kataKetiga)
+	fmt.Print(kataKeempat)
+
+	// soal 4
+
+	// soal 5
+
+}
